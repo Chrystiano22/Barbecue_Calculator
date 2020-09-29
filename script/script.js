3ce699cb8fcdf76f05d49df3f5471c337076e230
Chrystiano Mendes <angello.chrystiano@gmail.com>
2020-09-29 16:24:21 -0300

    Commit

--- script/script.js
+++ script/script.js
@@ -15,9 +15,9 @@ function Calculate() {
     let amountBeer = BeerByPerson(duration) * adults
     let amountDrink = DrinkByPerson(duration) * adults + (MeatByPerson(duration) / 2 * kids)
 
-    result.innerHTML = `<p>${amountMeat / 1000} Kg of meat </p>`
-    result.innerHTML += `<p>${Math.ceil(amountBeer / 355)} cans of beer </p>`
-    result.innerHTML += `<p>${Math.ceil(amountDrink / 2000)} bottles of soda and water </p>`
+    result.innerHTML = `<p>  ${amountMeat / 1000} Kg of meat <img src="https://img.icons8.com/cotton/36/000000/steak-rare--v1.png"/> </p>`
+    result.innerHTML += `<p>${Math.ceil(amountBeer / 355)} cans of beer <img src="https://img.icons8.com/doodle/36/000000/beer--v1.png"/> </p>`
+    result.innerHTML += `<p>${Math.ceil(amountDrink / 2000)} bottles of soda and water <img src="https://img.icons8.com/cotton/36/000000/orange-soda-bottle--v1.png"/> </p>`
 }
 
 function MeatByPerson(duration) {
